b88abc0c448ab5342a751cdfba6684bbe33615e7
Oleg Sachev <cbi444@gmail.com>
2020-09-03 03:23:08 +0300

    [task-2] Add separate file for settings storing.

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+const bookInfoWidth = 300;
+const bookInfoPadding = 10;
+const defaultAuthorsCount = 3;
+const authorInfoWidth = 120;
+const authorInfoPadding = 10;
+const authorsListWidth = defaultAuthorsCount * (authorInfoWidth + authorInfoPadding * 2);
+
+export {
+    bookInfoWidth,
+    bookInfoPadding,
+    defaultAuthorsCount,
+    authorInfoWidth,
+    authorInfoPadding,
+    authorsListWidth
+}
